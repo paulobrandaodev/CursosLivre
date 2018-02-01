10f30daae4ef7de7e2912511d884fe50cf4bc102
Paulo Brandão <32689830+paulobrandaooficial@users.noreply.github.com>
2018-02-01 21:10:35 -0200

    Conttrollers

--- Controllers/AreasController.cs
+++ Controllers/AreasController.cs
@@ -1,7 +1,78 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using CursosLivre.Dados;
+using CursosLivre.Models;
+
 namespace CursosLivre.Controllers
 {
-    public class AreasController
+
+    [Route("api/[controller]")]
+    public class AreasController : Controller
     {
+        Areas area = new Areas();
+        readonly CursosLivreContexto contexto;
+
+        public AreasController(CursosLivreContexto contexto){
+            this.contexto = contexto;
+        }
+
+        [HttpGet]
+        public IEnumerable<Areas> Listar(){
+            return contexto.Areas.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Areas Listar(int id){
+            return contexto.Areas.Where( x => x.IdArea == id).FirstOrDefault();
+        }
+
+        [HttpPost]
+        public void Cadastrar([FromBody] Areas cli){
+            contexto.Areas.Add(cli);
+            contexto.SaveChanges();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id,[FromBody] Areas area){
+            if(area == null || area.IdArea != id){
+                return BadRequest();
+            }
+            var cli = contexto.Areas.FirstOrDefault(x => x.IdArea == id);
+            if(cli == null){
+                return NotFound();
+            }
+
+            cli.IdArea     = area.IdArea;
+            cli.NomeArea   = area.NomeArea;
+
+            contexto.Areas.Update(cli);
+            int rs = contexto.SaveChanges();
+
+            if(rs > 0)
+            return Ok();
+            else
+            return BadRequest();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Apagar(int id){
+            var area = contexto.Areas.Where( x => x.IdArea == id).FirstOrDefault();
+            if(area == null){
+                return NotFound();
+            }
+
+            contexto.Areas.Remove(area);    
+            int rs = contexto.SaveChanges();
+
+            if(rs > 0)
+            return Ok();
+            else
+            return BadRequest();
+        }
         
+
     }
+        
 }
